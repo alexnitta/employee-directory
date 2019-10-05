e27e639a960a5e459bf24933512c40a8aab2a860
Alex Nitta <a.j.nitta@gmail.com>
2019-10-05 09:24:59 -0700

    Set up createEmployee mutation

--- server/src/api/resolvers/Mutation/createEmployee.js
+++ server/src/api/resolvers/Mutation/createEmployee.js
@@ -1,24 +1,20 @@
-const dgraph = require("dgraph-js");
+const dgraph = require('dgraph-js');
 
 async function createEmployee(parentValue, args, context) {
     const transaction = context.db.newTxn();
-    // TODO get these values from args
-    const employee = {
-        uid: '_:employeeId',
-        typeEmployee: '',
-        firstName: 'John',
-        lastName: 'Deer',
-        gender: 'MALE',
-        title: 'Mr.',
-        email: 'john@deer.com',
-    };
     const mutation = new dgraph.Mutation();
+    const employee = { ...args.input };
+
+    employee.uid = '_:employeeId',
+    employee.typeEmployee = '';
+
     mutation.setSetJson(employee);
 
     const response = await transaction.mutate(mutation);
 
     await transaction.commit();
-    const mutationUid = response.getUidsMap().get("employeeId");
+
+    const mutationUid = response.getUidsMap().get('employeeId');
 
     employee.uid = mutationUid;
 
