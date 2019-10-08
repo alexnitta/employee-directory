2b876185598e9c8eb575aa0ab0c38e37f882839f
Alex Nitta <a.j.nitta@gmail.com>
2019-10-08 06:22:22 -0700

    Improve styling on AddEmployee button

--- client/src/components/AddEmployee/AddEmployee.js
+++ client/src/components/AddEmployee/AddEmployee.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Button } from 'rendition';
+import styled from 'styled-components/macro';
+import { UserAdd } from 'grommet-icons';
 
 import { messages } from '../../locale/en_us';
 
 const { addEmployee } = messages;
 
+const ButtonWrapper = styled.div`
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-bottom: 20px;
+    text-align: center;
+`;
+
 export const AddEmployee = () => (
-    <Button primary>{addEmployee.callToAction}</Button>
+    <ButtonWrapper>
+        <Button tertiary icon={<UserAdd />}>
+            {addEmployee.callToAction}
+        </Button>
+    </ButtonWrapper>
 );
