f72ae264aa13b3040e17cfe5e9f4ca60735e5e9e
Alex Nitta <anitta@pixsystem.com>
2019-10-08 15:02:25 -0700

    Add ApolloProvider to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Provider as RenditionProvider, Flex } from 'rendition';
 import styled from 'styled-components/macro';
-import { Provider as ReduxProvider } from 'react-redux'
-import { createStore } from 'redux'
+import { Provider as ReduxProvider } from 'react-redux';
+import { createStore } from 'redux';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
 import { MainContent } from './components/MainContent';
 import { MainNav } from './components/MainNav';
@@ -13,6 +15,14 @@ const store = createStore(
     rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+const graphqlUri = process.env.NODE_ENV === 'development' ?
+    'http://localhost:8001/graphql' :
+    'production_url_goes_here';
+
+const client = new ApolloClient({
+    uri: graphqlUri,
+});
 
 const LeftCol = styled.div`
     flex: 0 1 auto;
@@ -29,15 +39,17 @@ function App() {
         <div className="app">
             <RenditionProvider>
                 <ReduxProvider store={store}>
-                    <MainNav />
-                    <Flex>
-                        <LeftCol>
-                            <Sidebar />
-                        </LeftCol>
-                        <RightCol>
-                            <MainContent />
-                        </RightCol>
-                    </Flex>
+                    <ApolloProvider client={client}>
+                        <MainNav />
+                        <Flex>
+                            <LeftCol>
+                                <Sidebar />
+                            </LeftCol>
+                            <RightCol>
+                                <MainContent />
+                            </RightCol>
+                        </Flex>
+                    </ApolloProvider>
                 </ReduxProvider>
             </RenditionProvider>
         </div>
