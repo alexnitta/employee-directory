6f37039d11427fe1779ad280f5cc52ed7b004a18
Alex Nitta <anitta@pixsystem.com>
2019-10-08 14:41:34 -0700

    Set up Redux store and basic session reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Provider, Flex } from 'rendition';
+import { Provider as RenditionProvider, Flex } from 'rendition';
 import styled from 'styled-components/macro';
+import { Provider as ReduxProvider } from 'react-redux'
+import { createStore } from 'redux'
 
 import { MainContent } from './components/MainContent';
 import { MainNav } from './components/MainNav';
 import { Sidebar } from './components/Sidebar';
+import { rootReducer } from './redux/reducers';
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const LeftCol = styled.div`
     flex: 0 1 auto;
@@ -19,17 +27,19 @@ const RightCol = styled.div`
 function App() {
     return (
         <div className="app">
-            <Provider>
-                <MainNav />
-                <Flex>
-                    <LeftCol>
-                        <Sidebar />
-                    </LeftCol>
-                    <RightCol>
-                        <MainContent />
-                    </RightCol>
-                </Flex>
-            </Provider>
+            <RenditionProvider>
+                <ReduxProvider store={store}>
+                    <MainNav />
+                    <Flex>
+                        <LeftCol>
+                            <Sidebar />
+                        </LeftCol>
+                        <RightCol>
+                            <MainContent />
+                        </RightCol>
+                    </Flex>
+                </ReduxProvider>
+            </RenditionProvider>
         </div>
     );
 }
