ee2d14d57f3fb83bf0a6aaae483b3943f3e14215
Alex Nitta <a.j.nitta@gmail.com>
2019-10-10 09:13:43 -0700

    Create AppContainer; use it to render hamburger menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,26 @@
 import React from 'react';
-import { Provider as RenditionProvider, Flex } from 'rendition';
-import styled from 'styled-components/macro';
+import { Provider as RenditionProvider } from 'rendition';
 import { Provider as ReduxProvider } from 'react-redux';
 import { createStore } from 'redux';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
-import { MainContent } from './components/MainContent';
-import { MainNav } from './components/MainNav';
-import { Sidebar } from './components/Sidebar';
 import { rootReducer } from './redux/reducers';
+import { AppContainer } from './components/AppContainer';
 
 const store = createStore(
     rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-const graphqlUri = process.env.NODE_ENV === 'development' ?
-    'http://localhost:8001/graphql' :
-    'production_url_goes_here';
+const graphqlUri =
+    process.env.NODE_ENV === 'development'
+        ? 'http://localhost:8001/graphql'
+        : 'production_url_goes_here';
 
 const client = new ApolloClient({
     uri: graphqlUri,
 });
-
-const LeftCol = styled.div`
-    flex: 1 1 auto;
-    max-width: 20%
-`;
-
-const RightCol = styled.div`
-    flex: 1 1 auto;
-`;
 
 function App() {
     return (
@@ -39,15 +28,7 @@ function App() {
             <ReduxProvider store={store}>
                 <ApolloProvider client={client}>
                     <RenditionProvider>
-                        <MainNav />
-                        <Flex>
-                            <LeftCol>
-                                <Sidebar />
-                            </LeftCol>
-                            <RightCol>
-                                <MainContent />
-                            </RightCol>
-                        </Flex>
+                        <AppContainer />
                     </RenditionProvider>
                 </ApolloProvider>
             </ReduxProvider>
--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Flex } from 'rendition';
 import styled from 'styled-components/macro';
 import { Menu } from 'grommet-icons';
@@ -15,36 +14,15 @@ const AppTitle = styled.h2`
     white-space: nowrap;
 `;
 
-const MenuWrapper = styled.div`
-    padding-left: 15px;
-`;
-
-const Sidebar = ({ sidebarOpen, dispatchSetSidebarOpen }) => {
-    return (
-        <div>
-            <MenuWrapper>
-                <Menu onClick={() => dispatchSetSidebarOpen(!sidebarOpen)} />
-            </MenuWrapper>
-            {sidebarOpen && <Flex flexDirection="column">
-                <Flex alignItems="center">
-                    <AppTitle>Employee Directory</AppTitle>
-                </Flex>
-                <AddEmployee />
-                {/*
-                    TODO: implement filters by department and office location
-                    <Filters />
-                */}
-            </Flex>}
-        </div>
-    );
-};
-
-const mapStateToProps = state => ({
-    sidebarOpen: sidebarOpenSelector(state),
-});
-
-const mapDispatchToProps = dispatch => ({
-    dispatchSetSidebarOpen: open => dispatch(setSidebarOpen({ open })),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
+export const Sidebar = () => (
+    <Flex flexDirection="column">
+        <Flex alignItems="center">
+            <AppTitle>Employee Directory</AppTitle>
+        </Flex>
+        <AddEmployee />
+        {/*
+            TODO: implement filters by department and office location
+            <Filters />
+        */}
+    </Flex>
+);
